f3d6bdc6adba9f0687b10895706f0afdb2453478
Eduardo Chavez <mccartney3150@gmail.com>
2020-08-01 21:33:15 +0000

    Initial commit

---
+++ Cubo.swift
@@ -0,0 +1,8 @@
+class Cubo {
+  var largo: Double = 0
+  var ancho: Double = 0
+
+  func mostrarValores() {
+    print("Largo \(largo), Ancho \(ancho)")
+  }
+}
---
+++ main.swift
@@ -0,0 +1,5 @@
+let cubo = Cubo()
+cubo.largo = 10
+cubo.ancho = 20
+
+cubo.mostrarValores()
